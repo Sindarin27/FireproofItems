a6dafa5068423f91bfd8873323f372c7560ef1d0
suppergerrie2 <15769860+suppergerrie2@users.noreply.github.com>
2023-09-30 15:29:18 +0200

    Update to 1.20.1

--- src/main/java/sindarin/fireproofitems/FireProofItemsMod.java
+++ src/main/java/sindarin/fireproofitems/FireProofItemsMod.java
@@ -2,10 +2,12 @@ package sindarin.fireproofitems;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -14,7 +16,8 @@ import net.minecraftforge.fml.common.Mod;
 public class FireProofItemsMod {
     public static final String MOD_ID = "fireproofitems";
 
-    public static final ResourceLocation itemTag = new ResourceLocation(MOD_ID, "fire_proof_items");
+    public static final ResourceLocation itemTagLocation = new ResourceLocation(MOD_ID, "fire_proof_items");
+    public static final TagKey<Item> itemTag = ItemTags.create(itemTagLocation);
 
     public FireProofItemsMod() {
         ModLoadingContext.get().registerConfig(net.minecraftforge.fml.config.ModConfig.Type.SERVER, ModConfig.serverSpec);
@@ -22,10 +25,11 @@ public class FireProofItemsMod {
     }
 
     @SubscribeEvent
-    public void onEntitySpawn(EntityJoinWorldEvent event) {
+    public void onEntitySpawn(EntityJoinLevelEvent event) {
         if (event.getEntity() instanceof ItemEntity) {
-            Item item = ((ItemEntity) event.getEntity()).getItem().getItem();
-            item.isFireResistant = ItemTags.getAllTags().getTagOrEmpty(itemTag).contains(item) == ModConfig.SERVER.isWhitelist.get();
+            ItemStack stack = ((ItemEntity) event.getEntity()).getItem();
+            Item item = stack.getItem();
+            item.isFireResistant = stack.is(itemTag) == ModConfig.SERVER.isWhitelist.get();
         }
     }
 }
